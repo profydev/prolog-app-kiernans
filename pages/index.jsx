40a4d3ae89eb04aa11b56f9337f6b51c44c818d5
kiernans <77819934+kiernans@users.noreply.github.com>
2022-10-31 17:46:12 -0700

    Landing page header (#16)
    * Add Links and Dashboard button
    
    * Add pricing, documentation and products routes
    
    * Connect links to routes
    
    * Change text color and size

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { Routes } from "@config/routes";
+import Link from "next/link";
+import { color, textFont } from "@styles/theme";
 
 const Header = styled.header`
   width: 100%;
@@ -28,13 +30,52 @@ const ContactButton = styled.button`
   }
 `;
 
+const LinksContainer = styled.div`
+  display: flex;
+  width: 420px;
+  justify-content: space-around;
+`;
+
+const HeaderAnchor = styled.a`
+  text-decoration: none;
+  color: ${color("gray", 500)};
+  ${textFont("md", "medium")};
+`;
+
+const DashboardButton = styled.button`
+  border-radius: 8px;
+  color: white;
+  background: #7f56d9;
+  border: 1px solid #7f56d9;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 8px;
+  width: 165px;
+  height: 44px;
+`;
+
 const IssuesPage = () => {
   return (
     <div>
       <Header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <LinksContainer>
+          <Link href={Routes.home} passHref>
+            <HeaderAnchor>Home</HeaderAnchor>
+          </Link>
+          <Link href={Routes.products} passHref>
+            <HeaderAnchor>Products</HeaderAnchor>
+          </Link>
+          <Link href={Routes.documentation} passHref>
+            <HeaderAnchor>Documentation</HeaderAnchor>
+          </Link>
+          <Link href={Routes.pricing} passHref>
+            <HeaderAnchor>Pricing</HeaderAnchor>
+          </Link>
+        </LinksContainer>
+        <a href={Routes.projects}>
+          <DashboardButton>Open Dashboard</DashboardButton>
+        </a>
       </Header>
       <ContactButton
         onClick={() =>
