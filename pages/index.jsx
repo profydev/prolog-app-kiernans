4161f73499e54b36534fead2c9f186d2f30c1494
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-10-31 20:06:52 -0700

    Create Modal and add styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Routes } from "@config/routes";
 import Link from "next/link";
-import { color, textFont } from "@styles/theme";
+import { color, space, textFont } from "@styles/theme";
 
 const Header = styled.header`
   width: 100%;
@@ -53,6 +53,95 @@ const DashboardButton = styled.button`
   height: 44px;
 `;
 
+const Modal = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 288px;
+  background: #ffffff;
+  box-shadow: 0px 20px 24px -4px rgba(16, 24, 40, 0.1),
+    0px 8px 8px -4px rgba(16, 24, 40, 0.04);
+  border-radius: 12px;
+  gap: ${space(8)};
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: ${space(5)};
+  width: 352px;
+  height: 164px;
+`;
+
+const ModalTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: ${space(2)};
+  width: 352px;
+  height: 96px;
+`;
+
+const ModalText = styled.div`
+  font-family: Inter;
+  color: ${color("gray", 900)};
+  ${textFont("lg", "medium")};
+  text-align: center;
+`;
+
+const ModalSubtext = styled(ModalText)`
+  color: ${color("gray", 500)};
+  ${textFont("sm", "regular")};
+`;
+
+const ModalImage = styled.img`
+  width: 44px;
+  height: 44px;
+`;
+
+const ModalActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: ${space(3)};
+
+  width: 352px;
+  height: 44px;
+`;
+
+const ModalButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0px;
+  width: 170px;
+  height: 44px;
+  border-radius: 8px;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 8px;
+  font-family: Inter;
+  ${textFont("md", "medium")};
+`;
+
+const ModalCancelButton = styled(ModalButton)`
+  background: #ffffff;
+  border: 1px solid #d0d5dd;
+  color: ${color("gray", 700)};
+`;
+
+const ModalEmailButton = styled(ModalButton)`
+  background: #7f56d9;
+  border: 1px solid #7f56d9;
+  color: white;
+`;
+
 const IssuesPage = () => {
   return (
     <div>
@@ -87,6 +176,24 @@ const IssuesPage = () => {
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
       </ContactButton>
+
+      <Modal>
+        <ModalContent>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <ModalImage src="/icons/email.svg" alt="Email Icon" />
+          <ModalTextContainer>
+            <ModalText>Contact Us Via Email</ModalText>
+            <ModalSubtext>
+              Any questions? Send us an email at prolog@profy.dev. We usually
+              answer within 24 hours.
+            </ModalSubtext>
+          </ModalTextContainer>
+        </ModalContent>
+        <ModalActions>
+          <ModalCancelButton>Cancel</ModalCancelButton>
+          <ModalEmailButton>Open Email App</ModalEmailButton>
+        </ModalActions>
+      </Modal>
     </div>
   );
 };
