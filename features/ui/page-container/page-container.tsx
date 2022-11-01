cbc8a5f2ba79c8dbcb31e495f4260ffb8f729525
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-10-31 21:39:58 -0700

    Create footer for projects list

--- features/ui/page-container/page-container.tsx
+++ features/ui/page-container/page-container.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styled from "styled-components";
 import { SidebarNavigation } from "@features/ui";
 import { color, displayFont, textFont, space, breakpoint } from "@styles/theme";
+import Link from "next/link";
 
 type PageContainerProps = {
   children: React.ReactNode;
@@ -52,6 +53,90 @@ const Info = styled.div`
   ${textFont("md", "regular")}
 `;
 
+const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+
+  width: 100%;
+  height: 60px;
+
+  background: ${color("gray", 50)};
+`;
+
+const FooterContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0px 32px;
+  gap: ${space(8)};
+
+  width: 100%;
+  height: 33px;
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+
+  width: 94%;
+  height: 33px;
+`;
+
+const FooterText = styled.div`
+  font-family: Inter;
+  ${textFont("md", "regular")};
+  color: ${color("gray", 400)};
+`;
+
+const FooterLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 0px;
+  gap: ${space(6)};
+  width: 28.5%;
+  height: 24px;
+`;
+
+const FooterLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: ${space(2)};
+
+  width: 39px;
+  height: 24px;
+`;
+
+const FooterAnchor = styled.a`
+  text-decoration: none;
+  font-family: Inter;
+  ${textFont("md", "medium")}
+  color: ${color("gray", 500)};
+`;
+
+const LogoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 0px;
+  width: 14%;
+  height: 33px;
+`;
+
+const Logo = styled.img`
+  width: 23px;
+  height: 33px;
+`;
+
 export function PageContainer({ children, title, info }: PageContainerProps) {
   return (
     <Container>
@@ -68,6 +153,30 @@ export function PageContainer({ children, title, info }: PageContainerProps) {
           <Info>{info}</Info>
           {children}
         </ContentContainer>
+        <Footer>
+          <FooterContainer>
+            <FooterContent>
+              <FooterText>Version: 14.5.1</FooterText>
+              <FooterLinks>
+                <FooterLink href="#" passHref>
+                  <FooterAnchor>Docs</FooterAnchor>
+                </FooterLink>
+                <FooterLink href="#" passHref>
+                  <FooterAnchor>API</FooterAnchor>
+                </FooterLink>
+                <FooterLink href="#" passHref>
+                  <FooterAnchor>Help</FooterAnchor>
+                </FooterLink>
+                <FooterLink href="#" passHref>
+                  <FooterAnchor>Community</FooterAnchor>
+                </FooterLink>
+              </FooterLinks>
+              <LogoWrap>
+                <Logo src="/icons/logo-small.svg" alt="Small Logo" />
+              </LogoWrap>
+            </FooterContent>
+          </FooterContainer>
+        </Footer>
       </Main>
     </Container>
   );
