59d8d68773d816958e7f9f50106de21586ec40ca
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-11-07 23:13:24 -0700

    Add styling for focused and option color

--- features/ui/select/select.tsx
+++ features/ui/select/select.tsx
@@ -47,13 +47,34 @@ const Options = styled.select<OptionsProps>`
   box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
   border-radius: ${space(2)};
   ${textFont("md", "regular")};
-  color: ${color("gray", 900)};
+  color: ${color("gray", 500)};
+
+  &:focus {
+    ${({ error }) =>
+      error
+        ? css`
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05),
+              0px 0px 0px 4px ${color("error", 100)};
+            outline: 0 !important;
+          `
+        : css`
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05),
+              0px 0px 0px 4px ${color("primary", 100)};
+            border: 1px solid ${color("primary", 300)};
+            outline: 0 !important;
+          `};
+  }
 
   ${({ error }) =>
     error &&
     css`
       border: 1px solid ${color("error", 300)};
     `};
+`;
+
+const Placeholder = styled.option`
+  ${textFont("md", "regular")};
+  color: ${color("gray", 500)};
 `;
 
 const Hint = styled.span`
@@ -66,8 +87,13 @@ const Error = styled.span`
   color: ${color("error", 500)};
 `;
 
+const StyledSelectOption = styled.option`
+  ${textFont("md", "regular")};
+  color: ${color("gray", 900)};
+`;
+
 const SelectOption = ({ option }: SelectOptionProps) => {
-  return <option>{option}</option>;
+  return <StyledSelectOption>{option}</StyledSelectOption>;
 };
 
 export const Select = ({
@@ -86,9 +112,10 @@ export const Select = ({
   return (
     <form>
       <Label>
-        <LabelText>{label}</LabelText>
+        {label && <LabelText>{label}</LabelText>}
         {options && options.length > 0 && (
-          <Options ref={optionsRef} error={error} {...selectProps}>
+          <Options ref={optionsRef} error={error} {...selectProps} required>
+            <Placeholder hidden>Select team member</Placeholder>
             {options.map((option: string, index) => (
               <SelectOption key={index} option={option} />
             ))}
