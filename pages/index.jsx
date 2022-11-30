2341540b4187f86d34b2b80c8d8f4e4bddf2798a
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-11-29 19:35:05 -0700

    Enable or disable scrolling on Modal state

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,49 +4,22 @@ import styled from "styled-components";
 import { space } from "@styles/theme";
 import { Section, useHome } from "../features/home";
 
-const HeroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 53.75rem;
-  gap: ${space(16)};
-  padding: ${space(24)}, 0px;
-`;
-
-const SocialContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid red;
-  width: 100%;
-  height: ${space(64)};
-  padding: ${space(24)}, 0px;
-`;
-
-const TestimonialContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid green;
-  width: 100%;
-  height: 50rem;
-  padding: ${space(24)}, 0px;
-`;
-
 const IssuesPage = () => {
   const [openModal, setOpenModal] = useState(false);
   const { data, isError, isLoading, error } = useHome();
 
   const handleCloseModal = () => {
     setOpenModal(false);
+    //Reenable scrolling
+    document.body.style.overflow = "unset";
   };
 
   const handleOpenModal = () => {
     setOpenModal(true);
+    //Scroll to top of page
     window.scrollTo({ top: 0, left: 0 });
+    //Disable scrolling
+    document.body.style.overflow = "hidden";
   };
 
   if (!isLoading) {
@@ -80,13 +53,10 @@ const IssuesPage = () => {
   return (
     <div>
       <Header />
-
       {/*eslint-disable-next-line no-undef */}
       <Section content={heroSection} />
-
       {/*eslint-disable-next-line no-undef */}
       <Section content={socialSection} />
-
       {/*eslint-disable-next-line no-undef */}
       <Section content={testimonialSection} openModal={handleOpenModal} />
       {openModal && <Modal closeModal={handleCloseModal} />}
